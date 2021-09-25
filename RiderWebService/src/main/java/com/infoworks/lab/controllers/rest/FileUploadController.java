681555aaffe1a352674a43c092987cae14a58a9c
Towhid Islam <m.towhid.islam@gmail.com>
2021-09-25 15:49:06 +0600

    how to download a file using spring mvc rest api

--- RiderWebService/src/main/java/com/infoworks/lab/controllers/rest/FileUploadController.java
+++ RiderWebService/src/main/java/com/infoworks/lab/controllers/rest/FileUploadController.java
@@ -1,27 +1,39 @@
 package com.infoworks.lab.controllers.rest;
 
 import com.infoworks.lab.rest.models.ItemCount;
+import com.infoworks.lab.services.iServices.iResourceService;
 import com.infoworks.lab.services.impl.LocalStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/files")
 public class FileUploadController {
 
     private LocalStorageService storageService;
+    private iResourceService resourceService;
+    private static Logger LOG = Logger.getLogger(FileUploadController.class.getSimpleName());
 
     @Autowired
-    public FileUploadController(@Qualifier("local") LocalStorageService storageService) {
+    public FileUploadController(@Qualifier("local") LocalStorageService storageService
+                , iResourceService resourceService) {
         this.storageService = storageService;
+        this.resourceService = resourceService;
     }
 
     @GetMapping("/rowCount")
@@ -46,6 +58,33 @@ public class FileUploadController {
         storageService.put(content.getOriginalFilename(), content.getInputStream());
         //storageService.save(false);
         return ResponseEntity.ok("Content Received: " + content.getOriginalFilename());
+    }
+
+    @GetMapping("/download")
+    public ResponseEntity<Resource> downloadContent(@RequestParam("fileName") String fileName) throws IOException {
+        /*String fileName = "11812130661623646424584651857.jpg";
+        File file = new File("sample/" + fileName);
+        LOG.info("File path: " + file.getAbsolutePath());
+        //
+        InputStream ios = resourceService.createStream(file);
+        int contentLength = file.length();
+        */
+        InputStream ios = storageService.read(fileName);
+        int contentLength = ios.available();
+        ByteArrayResource resource = new ByteArrayResource(resourceService.readAsBytes(ios));
+        ios.close();
+        //
+        HttpHeaders header = new HttpHeaders();
+        header.add(HttpHeaders.CONTENT_DISPOSITION, String.format("attachment; filename=%s", fileName));
+        header.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        header.add("Pragma", "no-cache");
+        header.add("Expires", "0");
+        //
+        return ResponseEntity.ok()
+                .headers(header)
+                .contentLength(contentLength)
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(resource);
     }
 
 }
